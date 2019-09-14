34c7783356364039df6e19f9117a6c75b3312648
Mroussell <55032397+Mroussell@users.noreply.github.com>
2019-09-14 17:56:13 -0400

    src file for arithmeic1

---
+++ arithmetic1.s
@@ -0,0 +1,16 @@
+@Arithmetic Program A=(A+B)-(C*D)
+.section .data
+.section .text
+.globl _start
+_start:
+	mov r1,#10
+	mov r2,#11
+	mov r3,#7
+	mov r4,#2
+	add r1,r2
+	mul r3,r4
+	sub r1,r3
+
+	mov r7,#1
+	svc #0
+.end
